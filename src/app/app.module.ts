45d337933dba9453e5c16f320ed529ba1a786cfc
Souleymane Sall <papa.sall243@gmail.com>
2023-10-18 12:57:40 +0200

    Initialisation angular project, create interface, services, component et affiche dans le navigateur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UserService } from './user/user.service';
+import { UserTableViewComponent } from './user/user-table-view/user-table-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTableViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/user-table-view/user-table-view.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-table-view',
+  templateUrl: './user-table-view.component.html',
+  styleUrls: ['./user-table-view.component.css']
+})
+export class UserTableViewComponent {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+  
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+}
